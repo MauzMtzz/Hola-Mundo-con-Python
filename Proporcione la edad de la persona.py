f7faea886736ec7497982b1a0d0ad852e653feba
MAUZ <maury3d@hotmail.com>
2026-03-03 18:12:20 -0600

    Add files via upload

---
+++ Proporcione la edad de la persona.py
@@ -0,0 +1,7 @@
+edadAdulto=18
+edadPersona=int(input('Proporcione la edad de la persona: '))
+
+if edadPersona>=edadAdulto:
+    print('La persona es un adulto')
+else:
+    print('La persona es un menor de edad')
